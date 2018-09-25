eb2e6b3d75a52ea01ea1dc5d799b1d969a1cb4c2
Werner Kroneman <werner@wernerkroneman.nl>
2018-09-26 00:39:49 +0200

    Implemented conversational interface. Implemented "help" command. Refactored DB stuff into db.py file, made interface to persistant stuff abstract.

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import asyncio
 import discord
 import shlex
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
 
 import db
 
@@ -11,8 +9,9 @@ PREFIX = '--'
 
 client = discord.Client()
 
-eng = db.mkEngine()
-getSession = sessionmaker(bind=eng)
+peristent = db.openPeristent()
+
+awaiting_answer = {}
 
 @client.event
 async def on_ready():
@@ -21,74 +20,86 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+def await_answer(in_response_to : discord.Message):
+
+    fut = asyncio.Future()
+
+    awaiting_answer[(in_response_to.channel.id, in_response_to.author.id)] = fut
+
+    return fut
+
+
+async def await_nonnegative_int(message):
+    attack = None
+    while not isinstance(attack, int) or attack < 0:
+        response = await await_answer(message)
+
+        try:
+            attack = int(response)
+        except ValueError:
+            await client.send_message(message.channel, "Please enter a non-negative integer.")
+
+    return attack
+
+HELP = '''
+Ganbot has the following commands:
+
+`$PREFIX$create_character`
+    Start the character creation process.
+    
+`$PREFIX$list_characters`
+    List all of your characters. (Other players' characters aren't listed.)
+
+`$PREFIX$help`
+    Display this message.
+'''.replace('$PREFIX$', PREFIX)
+
 @client.event
 async def on_message(message):
 
+    # Try so we can respond with a message to the user if something goes wrong
     try:
 
+        # The bot should ignore its' own messages.
+        if message.author.id == client.user.id:
+            return
 
+        # Check if the bot is waiting for a message for this user in this channel
+        awaiting = awaiting_answer.pop((message.channel.id, message.author.id), None)
+
+        # Yes! Go send the answer into that coroutine.
+        if awaiting is not None:
+            awaiting.set_result(message.content)
+            return
+
+        # Ignore anythong that doesn't start with the prefix.
         if not message.content.startswith(PREFIX):
             return
 
         print('Got command: ' + message.content)
 
+        # Slice off the prefix, then split the command into individual words.
+        # Words surrounded by "quotes" are left as one, wit the quotes stripped off.
         tokens = shlex.split(message.content[len(PREFIX):])
 
+        #Whoops... We got the prefix, then nothing!
         if tokens == []:
-            await client.send_message(message.channel, 'No command provided.')
-            # TODO Show some kind of help message
+            await client.send_message(message.channel, 'No command provided.\n'+HELP)
             return
 
+        # User wants to create a character!
         if tokens[0] == 'create_character':
-            # await client.send_message(message.channel, 'Woo! creating a character is fun!')
 
-            name = tokens[1]
-
-            character = db.Character(name=name,
-                                     player=message.author.id,
-                                     attack=5,
-                                     defence=5,
-                                     magic=5,
-                                     wisdom=5,
-                                     speed=5)
-
-            try:
-                session = getSession()
-                session.add(character)
-                session.commit()
-
-                feedback = 'Say hello to {}! ({})'.format(character.name, character.format_stats())
-
-                await client.send_message(message.channel, feedback)
-
-            except IntegrityError as ie:
-                x = ie.args[0]
-
-                if x == '(sqlite3.IntegrityError) UNIQUE constraint failed: users.name':
-                    errmsg = 'A character with the name {} already exists!'.format(character.name)
-                    await client.send_message(message.channel,
-                                              errmsg)
-
-                session.rollback()
+            await create_character(message)
 
         elif tokens[0] == 'list_characters':
-            session = getSession()
+            await list_characters(message)
 
-            characters = session.query(db.Character).filter_by(player=message.author.id).all()
-
-            if characters:
-                msg = 'Off the top of my head, I remember you had these characters:\n' +\
-                      '\n'.join(map(lambda x: '- ' + str(x),
-                                    characters))
-            else:
-                msg = 'You have no characters. Go make one!'
-
-            await client.send_message(message.channel, msg)
-
+        elif tokens[0] == 'help':
+            await client.send_message(message.channel, HELP)
 
         else:
-            await client.send_message(message.channel, 'Unrecognized command.')
-            # TODO Show help as well
+            await client.send_message(message.channel, 'Unrecognized command.\n'+HELP)
 
     except Exception as ex:
         await client.send_message(message.channel,
@@ -96,17 +107,73 @@ async def on_message(message):
 
         raise ex
 
-    # if message.content.startswith('!test'):
-    #     counter = 0
-    #     tmp = await client.send_message(message.channel, 'Calculating messages...')
-    #     async for log in client.logs_from(message.channel, limit=100):
-    #         if log.author == message.author:
-    #             counter += 1
-    #
-    #     await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    # elif message.content.startswith('!sleep'):
-    #     await asyncio.sleep(5)
-    #     await client.send_message(message.channel, 'Done sleeping')
+
+async def list_characters(message):
+    characters = peristent.getCharactersForUser(message.author.id)
+
+    if characters:
+        msg = 'Off the top of my head, I remember you had these characters:\n' + \
+              '\n'.join(map(lambda x: '- ' + str(x),
+                            characters))
+    else:
+        msg = 'You have no characters. Go make one!'
+    await client.send_message(message.channel, msg)
+
+
+async def create_character(message):
+    # Go through a series of prompts to define the character.
+
+    # Name
+    await client.send_message(message.channel,
+                              "Awoo! A new character! What are you gonna call them?")
+    name = await await_answer(message)
+    # Attack
+    await client.send_message(message.channel,
+                              "How strong is their attack? (Integer)")
+    attack = await await_nonnegative_int(message)
+    # Defence
+    await client.send_message(message.channel,
+                              "How strong is their defence? (Integer)")
+    defence = await await_nonnegative_int(message)
+    # Magic
+    await client.send_message(message.channel,
+                              "How strong is their magic? (Integer)")
+    magic = await await_nonnegative_int(message)
+    # Speed
+    await client.send_message(message.channel,
+                              "How fast are they? (Integer)")
+    speed = await await_nonnegative_int(message)
+    # Wisdom
+    await client.send_message(message.channel,
+                              "How wise are they? (Integer)")
+    wisdom = await await_nonnegative_int(message)
+    # Health
+    await client.send_message(message.channel,
+                              "How much much health do they have? (Integer)")
+    health = await await_nonnegative_int(message)
+    # Done! Package it up.
+    character = db.Character(name=name,
+                             player=message.author.id,
+                             attack=attack,
+                             defence=defence,
+                             magic=magic,
+                             wisdom=wisdom,
+                             speed=speed,
+                             health=health)
+
+    # Persist to database
+    try:
+        peristent.storeCharacter(character)
+
+        # Notify user that char has been created.
+        feedback = 'Say hello to {}! ({})'.format(character.name, character.format_stats())
+
+        await client.send_message(message.channel, feedback)
+
+    except db.DuplicateCharacterError:
+        errmsg = 'A character with the name {} already exists!'.format(character.name)
+        await client.send_message(message.channel, errmsg)
+
 
 if __name__ == '__main__':
     print('Starting Ganbot. Let the games begin!')
--- db.py
+++ db.py
@@ -2,6 +2,8 @@ import os.path
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 def mkEngine():
 
@@ -35,11 +37,46 @@ class Character(Base):
     speed = Column(Integer, nullable=False)
     magic = Column(Integer, nullable=False)
     wisdom = Column(Integer, nullable=False)
-    # health = Column(Integer)
+    health = Column(Integer, nullable=False)
 
     def __repr__(self):
        return '{} ({})'.format(self.name, self.format_stats())
 
     def format_stats(self):
-        return 'Attack: {} Defence: {} Magic: {} Wisdom: {} Speed: {}'\
-            .format(self.attack, self.defence, self.magic, self.wisdom, self.speed)
+        return 'Attack: {} Defence: {} Magic: {} Wisdom: {} Speed: {} Health: {}'\
+            .format(self.attack, self.defence, self.magic, self.wisdom, self.speed, self.health)
+
+def openPeristent():
+    return PeristentGanbot(mkEngine())
+
+def openTestPeristent():
+    return PeristentGanbot(mkTestEngine())
+
+class DuplicateCharacterError(Exception):
+    pass
+
+class PeristentGanbot():
+    '''
+    Class representing an abstract interface to persistent storage.
+    To SQLite, usually, but the caller is oblivious to this.
+    '''
+
+    def __init__(self, dbeng):
+        self.getSession = sessionmaker(bind=dbeng)
+
+    def storeCharacter(self, character: Character):
+        # Persist to database
+        session = self.getSession()
+
+        try:
+            session.add(character)
+            session.commit()
+
+        except IntegrityError as ie:
+            if ie.args[0] == '(sqlite3.IntegrityError) UNIQUE constraint failed: users.name':
+                raise DuplicateCharacterError()
+
+            raise ie
+
+    def getCharactersForUser(self, userId):
+        return self.getSession().query(Character).filter_by(player=userId).all()
